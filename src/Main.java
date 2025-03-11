eac5521166211b68c64172d7c3dbc6b5ff803c62
emeuvw <erikmeussen@volkerwessels.com>
2025-03-11 09:41:42 +0100

    Aligned closing bracket '}' nicely.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
+
      public static void main(String[] args) {
           System.out.print("Hello World!\n");
           System.out.print("This is the git repository version.\n");
+     }
 
-    }
 }
