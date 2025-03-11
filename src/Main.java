be90b2e959ce2eee6fb56505c79d884f1c980f70
emeuvw <erikmeussen@volkerwessels.com>
2025-03-11 09:39:43 +0100

    Splitted the output message in two separate 'System.out.print' calls.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello World!\nThis is the git repository version.\n");
+     public static void main(String[] args) {
+          System.out.print("Hello World!\n");
+          System.out.print("This is the git repository version.\n");
+
     }
 }
