a8831437332bb79a8cf66310e079183fe0ae52d1
emeuvw <erikmeussen@volkerwessels.com>
2025-03-11 09:17:42 +0100

    Update Main.java
    Made class Main, public.
    Updated the message to be printed to the console.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Hello World!\nThis is the git repository version.\n");
     }
 }
